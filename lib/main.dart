be6dcf1188990fbe365fb9d3b426f446d067e81f
ferika0702 <ferikaputra07@gmail.com>
2021-02-11 22:53:53 +0700

    ubah lagi gan

--- lib/main.dart
+++ lib/main.dart
@@ -94,7 +94,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Hello world (~Ferika Putra~)',
+              'Hello world (~MI2C-Ferika Putra~)',
             ),
             Text(
               '$_counter',
